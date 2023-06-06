6d3d456e00f377301a81e2accb548be283a305d3
ivaschenko <vaschenko@omnicomm.ru>
2023-06-06 19:49:51 +0300

    Chapter 8 - add some filters - add settings

---
+++ src/main/java/com/optimagrowth/gateaway/filters/TrackingFilter.java
@@ -0,0 +1,46 @@
+package com.optimagrowth.gateaway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Order(1)
+@Component
+public class TrackingFilter implements GlobalFilter {
+    private static final Logger logger = LoggerFactory.getLogger(TrackingFilter.class);
+
+    private final FilterUtils filterUtils;
+
+    public TrackingFilter(FilterUtils filterUtils) {
+        this.filterUtils = filterUtils;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+        if (isCorrelationIdPresent(requestHeaders)) {
+            logger.debug("tmx-correlation-id found in tracking filter: {}.", filterUtils.getCorrelationId(requestHeaders));
+        } else {
+            String corellationID = generateCorrelationId();
+            exchange = filterUtils.setCorrelationId(exchange, corellationID);
+            logger.debug("tmx-correlation-id generated in tracking filter: {}.", corellationID);
+        }
+        return chain.filter(exchange);
+    }
+
+    private String generateCorrelationId() {
+        return UUID.randomUUID().toString();
+    }
+
+    private boolean isCorrelationIdPresent(HttpHeaders requestHeaders) {
+        return filterUtils.getCorrelationId(requestHeaders) != null;
+    }
+}
